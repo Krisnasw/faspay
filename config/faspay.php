9679ca0821181acc6028c2e177e873cf04349d94
Krisnasw <krisna.457@gmail.com>
2017-08-28 13:18:15 +0700

    Update Package

---
+++ config/faspay.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'merchant_code'  => 'merchant-code',
+    
+    'merchant_name'  => 'merchant-name',
+    
+    'user_id' => 'faspay-userid',
+    
+    'password' => 'faspay-password',
+
+    'expiration_hours' => 24,
+];
